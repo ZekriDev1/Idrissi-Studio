323e06a81a241bfbc21bbdf0df9a7af6f73bb4c8
Zekri Akram <109292318+ZekriDev1@users.noreply.github.com>
2024-05-25 10:24:04 +0100

    Update

---
+++ test.ads
@@ -0,0 +1,12 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4258272119142105"
+     crossorigin="anonymous"></script>
+<!-- Adversaires -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-4258272119142105"
+     data-ad-slot="9893687684"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
